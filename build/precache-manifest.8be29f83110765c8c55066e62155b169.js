af0c1e718374982b86c3020c9223cd71f9dc488f
logan hill <loganhill@logans-MacBook-Pro.local>
2019-06-03 19:52:46 -0400

    moved portfolio from local storage

---
+++ build/precache-manifest.8be29f83110765c8c55066e62155b169.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354127def4d3d27b5074281049365522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00f2917fd2d4a707e5c4",
+    "url": "/static/css/main.17b54321.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "00f2917fd2d4a707e5c4",
+    "url": "/static/js/main.e394a92e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
